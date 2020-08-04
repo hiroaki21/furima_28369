c71c614ca7941bef88b2bc3644e6703b106b8752
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-04 22:48:03 +0900

    validationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
+  validates :email,uniqueness:{case_sensitive:false}
+  validates :password,           format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,message:"Include both letters and numbers"}
+  with_options presence:true do
+    validates :nickname
+    validates :last_name,        format:{with:/\A[ぁ-んァ-ン一-龥]/,message:"Full-width characters"}
+    validates :first_name,       format:{with:/\A[ぁ-んァ-ン一-龥]/,message:"Full-width characters"}
+    validates :last_name_kana,   format:{with:/\A([ァ-ン]|ー)+\z/,message:"Full-width katakana characters"}
+    validates :first_name_kana,  format:{with:/\A([ァ-ン]|ー)+\z/,message:"Full-width katakana characters"}
+    validates :birth_date
+  end
 end
