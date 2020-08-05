d256efb08ec70ee6c8db5fdf87eb5984e62a6056
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-05 19:17:09 +0900

    itemsテーブル　アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :fee_burden
+  belongs_to_active_hash :ship_origin
+  belongs_to_active_hash :ship_date
+
+  has_one :transaction
+  belongs_to :user
+  has_one :address
+
+  
 end
