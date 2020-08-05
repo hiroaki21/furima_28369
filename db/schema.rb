8b5432f1772b5b2b02f8517781e81cc7cff6e034
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-05 19:06:23 +0900

    itemテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_014549) do
+ActiveRecord::Schema.define(version: 2020_08_05_080800) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "introduce", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "fee_burden_id", null: false
+    t.integer "ship_origin_id", null: false
+    t.integer "ship_date_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_08_04_014549) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
---
+++ spec/factories/ship_origins.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :ship_origin do
+    
+  end
+end
