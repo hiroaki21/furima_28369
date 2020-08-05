131ed27f522375d42eb8bff53c7081e0e8e319d1
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-06 05:25:18 +0900

    createメソッドについて定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,4 +11,16 @@ class ItemsController < ApplicationController
       redirect_to "/users/sign_in"
     end
   end
+
+  def create
+    Item.create(item_params)
+    redirect_to :root
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title,:image,:introduce,:category_id,
+                                :status_id,:fee_burden_id,:ship_origin_id,
+                                :ship_date_id,:price).merge(user_id:current_user.id)
+  end
 end
