f6ae8893608eafaa8567a4a99630d65a8532b820
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-07 01:08:40 +0900

    単体テスト実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    title                     { Faker::Name.name }
+    introduce                 { Faker::Lorem.sentence}
+    category_id               { Faker::Number.within(range: 2..11)}
+    status_id                 { Faker::Number.within(range: 2..7)}
+    fee_burden_id             { Faker::Number.within(range: 2..3) }
+    ship_origin_id            { Faker::Number.within(range: 2..48) }
+    ship_date_id              { Faker::Number.within(range: 2..4) }
+    price                     { Faker::Number.within(range: 300..9999999) }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('public/images/test_image.png')
+    end
+
+    it 'ログインしていなければ、出品画面にいけない' do
+      
+    end
+
+    it 'titleが空では保存できないこと' do
+      @item.title = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Title can't be blank")
+    end
+
+    it 'imageが空では保存できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'introduceが空では保存できないこと' do
+      @item.introduce = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Introduce can't be blank")
+    end
+
+    it 'カテゴリが1では保存できないこと' do
+      @item.category_id = 1
+      @item.status_id = 1 
+      @item.fee_burden_id = 1 
+      @item.ship_origin_id = 1 
+      @item.ship_date_id = 1 
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select",
+              "Status Select",
+              "Fee burden Select",
+              "Ship origin Select",
+              "Ship date Select")
+    end
+
+    it 'priceが空では保存できないこと' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'priceが300以下では保存できないこと' do
+      @item.price = 288
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+
+    it 'priceが9999999より大きくては保存できないこと' do
+      @item.price = 100000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+
+    it 'priceが数字以外では保存できないこと' do
+      @item.price = "7.f"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+
+  end
 end
