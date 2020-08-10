c04a96dda453d8869842eab7adca019bebf5e245
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-10 11:49:26 +0900

    編集画面のページ遷移作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_login, only: [:new]
-  before_action :search_item, only: [:show]
+  before_action :search_item, only: [:show,:edit]
 
   def index
     @items = Item.all
@@ -20,6 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
   end
 
   private
