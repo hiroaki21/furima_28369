27c177c2ad3748d2a19b656dcce3a6761b3813c0
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-10 16:16:59 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :set_login, only: [:new]
-  before_action :search_item, only: [:show, :edit, :update]
+  before_action :set_login, only: [:new,:edit,:update,:create,:destroy]
+  before_action :search_item, only: [:show, :edit, :update,:destroy]
 
   def index
     @items = Item.all
@@ -31,6 +31,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to :root
   end
 
   private
